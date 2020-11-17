c73cd6f35672f8e622c52613223b8bb7f5f27df3
mdolding <Euph0n1um>
2020-11-17 15:59:38 +0000

    Add readonly file blob store

---
+++ src/stratus-gwc/src/main/java/org/geoserver/gwc/blobstore/readonlyfile/ReadOnlyFileBlobStoreInfo.java
@@ -0,0 +1,90 @@
+/**
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU Lesser General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * <p>Copyright 2019
+ */
+package org.geoserver.gwc.blobstore.readonlyfile;
+
+import org.geowebcache.config.BlobStoreInfo;
+import org.geowebcache.layer.TileLayerDispatcher;
+import org.geowebcache.locks.LockProvider;
+import org.geowebcache.storage.BlobStore;
+import org.geowebcache.storage.StorageException;
+
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * Mirrors FileBlobStoreInfo but for readonly use with block size removed.
+ * @author mike.dolding
+ */
+public class ReadOnlyFileBlobStoreInfo extends BlobStoreInfo  {
+
+    private static final long serialVersionUID = 87243591561561659L;
+
+    private String baseDirectory;
+
+    public ReadOnlyFileBlobStoreInfo() {
+        super();
+    }
+
+    public ReadOnlyFileBlobStoreInfo(String id) {
+        super(id);
+    }
+
+    /**
+     * Get the base directory for persisting tiles
+     *
+     * @return The file system path to the base directory
+     */
+    public String getBaseDirectory() {
+        return baseDirectory;
+    }
+
+    /**
+     * Set the base directory for persisting tiles
+     *
+     * @param baseDirectory The file system path to the base directory
+     */
+    public void setBaseDirectory(String baseDirectory) {
+        this.baseDirectory = baseDirectory;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("ReadOnlyFileBlobStore[id:")
+                .append(getName())
+                .append(", enabled:")
+                .append(isEnabled())
+                .append(", baseDirectory:")
+                .append(baseDirectory)
+                .append(']')
+                .toString();
+    }
+
+    /** @see BlobStoreInfo#createInstance(TileLayerDispatcher, LockProvider) */
+    @Override
+    public BlobStore createInstance(TileLayerDispatcher layers, LockProvider lockProvider)
+            throws StorageException {
+        checkState(getName() != null, "id not set");
+        checkState(
+                isEnabled(),
+                "Can't call ReadOnlyFileBlobStoreConfig.createInstance() is blob store is not enabled");
+        checkState(baseDirectory != null, "baseDirectory not provided");
+        ReadOnlyFileBlobStore fileBlobStore = new ReadOnlyFileBlobStore(baseDirectory);
+        return fileBlobStore;
+    }
+    /** @see BlobStoreInfo#getLocation() */
+    @Override
+    public String getLocation() {
+        return getBaseDirectory();
+    }
+}
